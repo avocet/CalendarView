b6f11e07c33fcf0e3cd8efa058440f2a7a024c94
avocet <avocet@me.com>
2025-01-30 01:13:36 +0800

    Update MonthView.swift

--- Sources/Internal/Views/MonthView.swift
+++ Sources/Internal/Views/MonthView.swift
@@ -23,7 +23,7 @@ struct MonthView: View {
             ForEach(data.items, id: \.last, content: createSingleRow)
         }
         .frame(width:UIScreen.main.bounds.width - 40)
-        .frame(maxHeight: .infinity)
+        .frame(height: 350)
         .animation(animation, value: selectedDate)
         .animation(animation, value: selectedRange?.getRange())
     }
